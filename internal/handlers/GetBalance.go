67795bc6d459fcaa283a9185f145eb6fecb09609
Naeel <tazet@narod.ru>
2025-02-14 13:42:13 +0300

    qu !

---
+++ internal/handlers/GetBalance.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Repinoid/ku/internal/models"
+	"github.com/Repinoid/ku/internal/securitate"
+)
+
+type BalanceStruct struct {
+	Current   float64 `json:"current"`
+	Withdrawn float64 `json:"withdrawn"`
+}
+
+func GetBalance(rwr http.ResponseWriter, req *http.Request) {
+
+	UserID, err := securitate.DataBase.LoginByToken(rwr, req)
+	if err != nil {
+		return
+	}
+
+	// tokenStr := req.Header.Get("Authorization")
+	// tokenStr, niceP := strings.CutPrefix(tokenStr, "Bearer <") // обрезаем -- Bearer <token>
+	// tokenStr, niceS := strings.CutSuffix(tokenStr, ">")
+
+	// //	var UserID int64
+	// //	err := DataBase.GetIDByToken(context.Background(), tokenStr, &UserID)	// получаем ID пользователя по полученному токену
+
+	// if (!niceP) || (!niceS) || (securitate.DataBase.GetIDByToken(context.Background(), tokenStr, &UserID) != nil) { // если неверная строка в Authorization - до GetIDByToken дело не дойдёт
+	// 	rwr.WriteHeader(http.StatusUnauthorized)            // 401 — неверная пара логин/пароль;
+	// 	fmt.Fprintf(rwr, `{"status":"StatusUnauthorized"}`) // либо токена неверный формат, либо по нему нет юзера в базе
+	// 	models.Sugar.Debug("Authorization header\n")
+	// 	return
+	// }
+
+	// order := "SELECT (SELECT SUM(o.accrual) FROM orders O where o.usercode=$1) as current, " +
+	// 	"(SELECT COALESCE(SUM(w.amount),0) FROM withdrawn w where w.usercode=$1) as withdrawn;"
+	order := "SELECT (SELECT SUM(orders.accrual) FROM orders where orders.usercode=$1), " +
+		"(SELECT COALESCE(SUM(withdrawn.amount),0) FROM withdrawn  where withdrawn.usercode=$1) ;"
+
+	row := securitate.DataBase.DB.QueryRow(context.Background(), order, UserID)
+	var current, withdr float64
+	err = row.Scan(&current, &withdr)
+	if err != nil {
+		rwr.WriteHeader(http.StatusInternalServerError) // //500 — внутренняя ошибка сервера.
+		fmt.Fprintf(rwr, `{"status":"StatusInternalServerError"}`)
+		models.Sugar.Debugf("row.Scan %+v\n", err)
+		return
+	}
+
+	bs := BalanceStruct{Current: current - withdr, Withdrawn: withdr} // текущий счёт - сумма бонусов минус сумма списаний
+
+	rwr.WriteHeader(http.StatusOK)
+	json.NewEncoder(rwr).Encode(bs)
+}
---
+++ internal/handlers/GetOrders.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Repinoid/ku/internal/models"
+	"github.com/Repinoid/ku/internal/securitate"
+)
+
+type OrdStruct struct {
+	Number      string  `json:"number"`
+	Status      string  `json:"status"`
+	Accrual     float64 `json:"accrual"`
+	Uploaded_at string  `json:"uploaded_at"`
+}
+
+func GetOrders(rwr http.ResponseWriter, req *http.Request) {
+
+	UserID, err := securitate.DataBase.LoginByToken(rwr, req)
+	if err != nil {
+		return
+	}
+
+	db := securitate.DataBase.DB
+	order := "select ordernumber as number, orderstatus as status, accrual, uploaded_at from orders where usercode=$1 order by uploaded_at ;"
+
+	rows, err := db.Query(context.Background(), order, UserID) //
+	if err != nil {
+		rwr.WriteHeader(http.StatusInternalServerError) //500 — внутренняя ошибка сервера.
+		fmt.Fprintf(rwr, `{"status":"StatusInternalServerError"}`)
+		models.Sugar.Debugf("db.Query %+v\n", err)
+		return
+	}
+
+	ord := OrdStruct{}
+	orda := []OrdStruct{}
+	var errScan error
+	for rows.Next() {
+		var tm time.Time
+		errScan = rows.Scan(&ord.Number, &ord.Status, &ord.Accrual, &tm)
+		ord.Uploaded_at = tm.Format(time.RFC3339)
+		if errScan != nil {
+			break
+		}
+		orda = append(orda, ord)
+	}
+	rows.Close()
+	if err := rows.Err(); err != nil || errScan != nil { // Err returns any error that occurred while reading. Err must only be called after the Rows is closed
+		rwr.WriteHeader(http.StatusInternalServerError) // //500 — внутренняя ошибка сервера.
+		fmt.Fprintf(rwr, `{"status":"StatusInternalServerError"}`)
+		models.Sugar.Debugf("db.Query %+v\n", err)
+		return
+	}
+	if len(orda) == 0 {
+		rwr.WriteHeader(http.StatusNoContent) // 204 No Content — сервер успешно обработал запрос, но в ответе были переданы только заголовки без тела сообщения
+		fmt.Fprintf(rwr, `{"status":"StatusNoContent"}`)
+		models.Sugar.Debug("No ORDERS\n")
+		return
+	}
+	rwr.WriteHeader(http.StatusOK)
+	//	fmt.Fprintf(rwr, `{"status":"StatusOK"}`)
+	json.NewEncoder(rwr).Encode(orda)
+}
