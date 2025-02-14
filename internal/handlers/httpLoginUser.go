b07a9b068c5bc32934434b3f7513bd7cca4ec646
Naeel <tazet@narod.ru>
2025-02-14 17:02:03 +0300

    up

--- internal/handlers/httpLoginUser.go
+++ internal/handlers/httpLoginUser.go
@@ -70,7 +70,7 @@ func LoginUser(rwr http.ResponseWriter, req *http.Request) {
 		models.Sugar.Debugf("UpdateToken %+v\n", err)
 		return
 	}
-	rwr.Header().Set("Authorization", "Bearer <"+Token+">")
+	rwr.Header().Add("Authorization", "Bearer <"+Token+">")
 	tok := struct {
 		Token string
 		Until time.Time
